0674482e4cfb9b446f0ff4fe5cf2d2874d94563b
David Acha Oliveira <125604583+davidacha@users.noreply.github.com>
2024-09-27 12:59:13 +0200

    Ejercicio 8

--- src/main/java/com/campusdual/classroom/Exercise08.java
+++ src/main/java/com/campusdual/classroom/Exercise08.java
@@ -14,7 +14,7 @@ public class Exercise08 {
     public static void whileLoop(int num) {
         int x = 0;
         while (x < num) {
-            System.out.println(x + " < " + num + ". El proximo ciclo valdra: "+x+1);
+            System.out.println(x + " < " + num + ". El proximo ciclo valdra: "+(x+1));
             x++;
         }
     }
